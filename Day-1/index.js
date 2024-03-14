0838de5ab515a338a1300e00cffe36c79ab1de20
Ankit Ojha <ankit1409@gmail.com>
2024-03-14 12:15:47 +0530

    Day-1 started

---
+++ Day-1/index.js
@@ -0,0 +1,5 @@
+console.log("hello Javascript");
+
+console.log('====================================');
+console.log(`Let's Start with 15 days of Javascript Challenege!!!`);
+console.log('====================================');
