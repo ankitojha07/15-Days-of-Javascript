32cd511726ffb74c2beaf2b5ca37cd926263d6a9
Ankit Ojha <ankit1409@gmail.com>
2024-03-15 14:20:38 +0530

    switch case added in day 1

--- ankitojhacoding/Day-1/conditional-statements.js
+++ ankitojhacoding/Day-1/conditional-statements.js
@@ -9,3 +9,31 @@ if(age >= 18){
 } else{
     console.log(`You have to wait for ${18-age} years to be eligible to vote.`);
 }
+
+// switch case in javascript
+
+var rank = "Commander";
+switch(rank)
+{
+    case "Private":
+    case "Sergeant":
+        console.log("You are not authorized.");
+        break;
+    case "Commander":
+        console.log("Hello commander! what can I do for you today?");
+        break;
+    case "Captain":
+        console.log("Hello captain! I will do anything you wish.");
+        break;
+    default:
+        console.log("I don't know what your rank is.");
+        break;
+}
+
+// looping statements
+
+let arr = [1,45,76,8,9,6,4,3];
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+}
