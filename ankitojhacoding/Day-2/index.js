c7585dc990588c74f4bfbef46de3425cf92c3166
Ankit <ankitojha1409@gmail.com>
2024-03-16 00:35:11 +0530

    high order functions in js

--- ankitojhacoding/Day-2/index.js
+++ ankitojhacoding/Day-2/index.js
@@ -1,1 +1,22 @@
-console.log('Hello Day2');
+console.log('Hello Day 2');
+
+// functions in Javascript
+
+function add(a,b){
+    let result = a+b;
+    return result;
+}
+
+let sum = add(12,45);
+console.log(sum);
+
+// how to pass callback function to a function
+
+
+// high order function -> cb is a callback function
+function sumOf(num1, num2, addMore){
+    let result = num1+num2;
+    if(result > 50) addMore(result)
+}
+
+let finalRes = sumOf(10,+45, (val)=>console.log(val+100));
