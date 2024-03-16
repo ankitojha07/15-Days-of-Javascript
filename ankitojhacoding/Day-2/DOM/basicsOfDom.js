597bdd58f7c8cda607b11a923f1c7d8fd920989a
Ankit <ankitojha1409@gmail.com>
2024-03-16 12:18:00 +0530

    DOM manipulation started

---
+++ ankitojhacoding/Day-2/DOM/basicsOfDom.js
@@ -0,0 +1,9 @@
+
+// accessing HTML elements using DOM in Javascript
+
+// Document object
+console.log(document.body);
+
+// selecting elelment using querySelector
+let div = document.querySelector("div");
+
