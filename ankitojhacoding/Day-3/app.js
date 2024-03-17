2ad3095f1fb937f2dbb70082f70c970baaa97818
Ankit <ankitojha1409@gmail.com>
2024-03-17 15:35:52 +0530

    Day-3 completed with EventListeners

--- ankitojhacoding/Day-3/app.js
+++ ankitojhacoding/Day-3/app.js
@@ -1,3 +1,28 @@
 
 
 // manipulating HTML through JS
+
+// function handleBtnClick(){
+//     console.log('Clicked');
+//     alert('You have just clicked me')
+// }
+
+// const btn1 = document.getElementById("btnClick")
+// btn1.addEventListener("click", function(){
+//     console.log('Clicked again');
+// })
+
+// same call back function as arrow function
+const btn2 = document.getElementById("btnClick")
+const nameBlock = document.getElementById('name-block')
+
+// btn2.addEventListener("click", ()=> {
+//     nameBlock.style.color = "blue"
+// })
+
+const container = document.getElementById('my-container');
+btn2.addEventListener("click", ()=>{
+    const el = document.createElement("h1")
+    el.innerHTML = "Ankit Ojha Coding"
+    container.appendChild(el)
+})
