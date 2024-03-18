c563dfd9ac5ea314e5b686040d7481a1c680718d
Ankit <ankitojha1409@gmail.com>
2024-03-18 13:26:07 +0530

    day 4 done

--- ankitojhacoding/Day-4/app.js
+++ ankitojhacoding/Day-4/app.js
@@ -12,16 +12,35 @@ let sum = a +b;
 //     console.log(restults);
 // }
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then((data)=>{
-    console.log(data.json());
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then((data)=>{
+//     console.log(data.json());
+// })
+// .catch((e)=>{
+//     console.log(e);
+// })
+// .finally(()=>{
+//     console.log('You data has been fetched successfully');
+// })
+
+// // getData();
+// console.log(sum);
+
+// creating custom promise
+
+
+let p = new Promise((resolve, reject)=>{
+    let num = 11;
+    if(num === 10){
+        resolve('Num is 10')
+    } else{
+        reject('Error... not 10')
+    }
+});
+
+p.then((d)=>{
+    console.log(d);
 })
 .catch((e)=>{
     console.log(e);
 })
-.finally(()=>{
-    console.log('You data has been fetched successfully');
-})
-
-// getData();
-console.log(sum);
