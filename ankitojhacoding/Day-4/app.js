b4aeb206895af9c39f4ba0b713f021bf5ca51c23
Ankit <ankitojha1409@gmail.com>
2024-03-18 13:13:44 +0530

    inbuilt promises in js

--- ankitojhacoding/Day-4/app.js
+++ ankitojhacoding/Day-4/app.js
@@ -1,3 +1,27 @@
 
 // Synchronous and Asynchronous JS
 
+let a = 10;
+let b = 20;
+
+let sum = a +b;
+
+
+// async function getData(){
+//     let restults = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     console.log(restults);
+// }
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((data)=>{
+    console.log(data.json());
+})
+.catch((e)=>{
+    console.log(e);
+})
+.finally(()=>{
+    console.log('You data has been fetched successfully');
+})
+
+// getData();
+console.log(sum);
