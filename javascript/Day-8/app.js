809d8e3fa3d5e0f7a3d783ec8dac7000cb662ecb
Ankit <ankitojha1409@gmail.com>
2024-03-22 14:58:02 +0530

    Day 8 Arrow functions in javascript

---
+++ javascript/Day-8/app.js
@@ -0,0 +1,23 @@
+
+// function sum(a,b){
+//     let sum = a+b;
+//     console.log('Sum is ', sum);
+// }
+
+// sum(14,2)
+
+// Arrow functions in javascript
+
+// const sum1 = (a,b)=> {
+//     let sum3 = a+b;
+//     console.log('Result from arrow function ', sum3);
+//     return sum3;
+// }
+
+// const sum1 = ()=> ({'name1':'Ankit'})
+
+const sum1 = ()=>{
+    return {'name1':'Ankit'}
+}
+console.log(sum1());
+
