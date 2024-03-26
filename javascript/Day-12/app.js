238a9a9b513c44f0771b0d12ec24a75400d95a4d
Ankit <ankitojha1409@gmail.com>
2024-03-26 22:47:30 +0530

    day 12 completed

--- javascript/Day-12/app.js
+++ javascript/Day-12/app.js
@@ -2,3 +2,25 @@
 
 // "this" keyword in javascript
 
+// const obj = {
+//     count :123,
+//     func : function(){
+//         return this.count
+//     }
+// }
+
+// console.log(obj.func());
+
+
+// function test(){
+//     console.log(this);
+// }
+
+// test()
+
+function Video(title){
+    this.title = title
+    console.log(this.title);
+}
+
+const v = new Video('day 12')
