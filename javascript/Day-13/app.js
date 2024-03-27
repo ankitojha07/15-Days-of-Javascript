fec7dc99d410de55ec432cde6276e051c8b110d1
Ankit <ankitojha1409@gmail.com>
2024-03-27 23:55:33 +0530

    map functions in javascript

--- javascript/Day-13/app.js
+++ javascript/Day-13/app.js
@@ -1,1 +1,12 @@
 // day 13- 15 Days of Javascript
+
+// map in javascript
+const arr = [10,45,78,9,6,3,5,7];
+
+const newArr = arr.map((ele)=>{
+    return ele*2;
+})
+
+newArr.forEach((e)=>{
+    console.log(e);
+})
