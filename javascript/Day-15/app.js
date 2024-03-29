a6b968f4e89a20651484d2acd4e2abe46bec5569
Ankit <ankitojha1409@gmail.com>
2024-03-30 01:57:35 +0530

    day 15 completed

---
+++ javascript/Day-15/app.js
@@ -0,0 +1,15 @@
+// Day-15 : 15 Days of Javascript
+
+// == vs === in Javascript
+
+// console.log(12=='12');
+
+// console.log(12==='12');
+
+// console.log(true === 1);
+// console.log(true === '1');
+
+console.log(false===0);
+console.log(false==='0');
+console.log(false=='');
+console.log(null === undefined);
