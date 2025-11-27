fda0734beea7a1b37d629348317ee4ea16b5ebec
Ankit Ojha <ankitojha1409@gmail.com>
2025-11-27 23:32:58 +0530

    rmdirSync, removing dirs using js fs

--- nodejs-2026/index.js
+++ nodejs-2026/index.js
@@ -13,17 +13,27 @@ console.log(data);
 // fs.writeFileSync("copy.txt", "I want to write this usig the js code.", "utf-8")
 
 // To append the content, use appendFileSync
-fs.appendFileSync("copy.txt", "this is the content to appned using the append file sync in js.", "utf-8")
-const readNew = fs.readFileSync("copy.txt", "utf-8")
-console.log(readNew);
+// fs.appendFileSync("copy.txt", "this is the content to appned using the append file sync in js.", "utf-8")
+// const readNew = fs.readFileSync("copy.txt", "utf-8")
+// console.log(readNew);
 
 
 // how to create a directory?
 // fs.mkdirSync("games")
 
 // now you want to Hierarchy of folders inside this?
-fs.mkdirSync("games/xyz/a",{recursive:true})
-fs.mkdirSync("games/xyz/b",{recursive:true})
-fs.mkdirSync("games/second",{recursive:true})
-fs.mkdirSync("games/second/1",{recursive:true})
-fs.mkdirSync("games/second/2",{recursive:true})
+// fs.mkdirSync("games/xyz/a",{recursive:true})
+// fs.mkdirSync("games/xyz/b",{recursive:true})
+// fs.mkdirSync("games/second",{recursive:true})
+// fs.mkdirSync("games/second/1",{recursive:true})
+// fs.mkdirSync("games/second/2",{recursive:true})
+
+// how to delete a directory?
+// fs.rmdirSync("games") // we cannot delete a directory which is not empty, so first delete the innner ones
+// fs.rmdirSync("games/second/1")
+// fs.rmdirSync("games/second/2")
+// fs.rmdirSync("games/second")
+
+// fs.rmdirSync("games/xyz/a")
+// fs.rmdirSync("games/xyz/b")
+// fs.rmdirSync("games/xyz")
