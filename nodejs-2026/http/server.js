04d11e344e18704c8c41fc2270f20bbf16e2596d
Ankit Ojha <ankitojha1409@gmail.com>
2025-11-29 01:40:57 +0530

    playing with the https requests

---
+++ nodejs-2026/http/server.js
@@ -0,0 +1,27 @@
+const http = require ("node:http")
+
+const PORT = 8000;
+const server = http.createServer((req,res)=>{
+    // all the DB operations goes here!!!
+console.log(req.url);
+console.log(req.method);
+
+switch(req.url){
+    case '/':
+        res.writeHead(200);
+        return res.end(`Homepage`)
+    
+    case '/contact-us':
+        res.writeHead(200);
+        return res.end("you can contact me at ankitojha1409@gmail.com")
+
+    default:
+        res.writeHead(404);
+        res.end("You are lost!")
+}
+
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server is up an running at port: ${PORT} `);
+})
