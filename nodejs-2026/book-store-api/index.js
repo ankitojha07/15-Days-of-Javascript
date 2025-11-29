bf0c52a6d4bb2e4666a749e76f40b782860e0633
Ankit Ojha <ankitojha1409@gmail.com>
2025-11-30 02:15:37 +0530

    delete book by id, implemented

--- nodejs-2026/book-store-api/index.js
+++ nodejs-2026/book-store-api/index.js
@@ -1,41 +1,75 @@
-import express from "express";
+import express, { json } from "express";
 
 const app = express()
 const port = 3030;
 
-const books = [
-    {"id":1, title:"Book 1", author:"Author 1"},
-    {"id":2, title:"Book 2", author:"Author 2"},
-    {"id":3, title:"Book 3", author:"Author 3"},
+const Books = [
+    { "id": 1, title: "Book 1", author: "Author 1" },
+    { "id": 2, title: "Book 2", author: "Author 2" },
+    { "id": 3, title: "Book 3", author: "Author 3" },
 ]
 
-app.get("/", (req,res)=>{
-    res.status(200).json(books)
+// Middleware - (plugin)
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json(Books)
 })
 
-app.get('/books', (req,res)=>{
-    res.status(200).json(books)
+// Implement get requests to get Books
+
+app.get('/books', (req, res) => {
+    res.status(200).json(Books)
 })
 
-app.get('/books/:id',(req,res)=>{
+// get book by Id
+
+app.get('/books/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    if(isNaN(id)){
-        return res.status(400).json({error:"Id must be of type number."})
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "Id must be of type number." })
     }
-
-    const book = books.find((e)=>e.id === id);
-
-    
-
-    if(!book){
-        return res 
-        .status(404)
-        .json({error:`Book with ${id} does not exist!`})
+    const book = Books.find((e) => e.id === id);
+    if (!book) {
+        return res
+            .status(404)
+            .json({ error: `Book with ${id} does not exist!` })
     }
-
     return res.json(book)
 })
 
-app.listen(port, ()=>{
+// Implement POST requests to add Books
+
+app.post("/books", (req, res) => {
+
+    const { title, author } = req.body;
+    if (!title || title === '') return res.status(400).json({ error: "Title is required" })
+    if (!author || author === '') return res.status(400).json({ error: "Author is required" })
+
+    const id = (Books.length)+1;
+    const book = { id, title, author };
+    Books.push(book)
+    console.log(Books);
+    return res.status(201).json({ Message: "Book has been added to the store Successfully!", id })
+})
+
+// delete a book by id
+
+app.delete("/books/:id", (req,res)=>{
+    const id = parseInt(req.params.id);
+    if(isNaN(id)){
+        return res.status(400).json({error:"Id must be a number!"})
+    }
+    const indexToDelete = Books.findIndex((e)=> e.id === id);
+
+    if(indexToDelete<0){
+        return res.status(404).json({error:`The id ${id} does not exists in our database`})
+    }
+
+    Books.splice(indexToDelete, 1);
+    return res.status(200).json({message:`You have successfully deleted the book with id ${id}!`})
+})
+
+app.listen(port, () => {
     console.log(`Server up and running at port ${port}`);
 })
