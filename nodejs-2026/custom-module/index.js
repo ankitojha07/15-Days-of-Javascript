ad6efa17f1d2722a0fbc81237d9e3211b9c7545a
Ankit Ojha <ankitojha1409@gmail.com>
2025-12-04 12:21:55 +0530

    importing custom modules

--- nodejs-2026/custom-module/index.js
+++ nodejs-2026/custom-module/index.js
@@ -1,2 +1,3 @@
 const value = require("./math")
 console.log(value.add(3,4));
+console.log(value.sub(5,3));
--- nodejs-2026/custom-module/math.js
+++ nodejs-2026/custom-module/math.js
@@ -1,3 +1,7 @@
 exports.add = function add(a,b){
     return a+b;
 }
+
+exports.sub = function sub(a,b){
+    return a-b;
+}
