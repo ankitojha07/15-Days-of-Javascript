3eb145bf6b21299708d91021ff84b5caf4c18653
Ankit Ojha <ankitojha1409@gmail.com>
2025-12-28 01:49:24 +0530

    updated default exports in book controllers

--- nodejs-2026/book-store-api/controllers/book.controller.js
+++ nodejs-2026/book-store-api/controllers/book.controller.js
@@ -1,47 +1,54 @@
-const Books = require("../models/books.js")
+import { Books } from "../models/books.js";
 
-exports.getAllBooks = (req,res)=>{
-    res.json(Books)
+export function getAllBooks(req, res) {
+  res.json(Books);
 }
 
-exports.getBookById = (req,res)=>{
-    const id = parseInt(req.params.id);
-    if (isNaN(id)) {
-        return res.status(400).json({ error: "Id must be of type number." })
-    }
-    const book = Books.find((e) => e.id === id);
-    if (!book) {
-        return res
-            .status(404)
-            .json({ error: `Book with ${id} does not exist!` })
-    }
-    return res.json(book)
+export function getBookById(req, res) {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Id must be of type number." });
+  }
+  const book = Books.find((e) => e.id === id);
+  if (!book) {
+    return res.status(404).json({ error: `Book with ${id} does not exist!` });
+  }
+  return res.json(book);
 }
 
-exports.createBooks = (req,res)=>{
+export function createBooks(req, res) {
+  const { title, author } = req.body;
+  if (!title || title === "")
+    return res.status(400).json({ error: "Title is required" });
+  if (!author || author === "")
+    return res.status(400).json({ error: "Author is required" });
 
-    const { title, author } = req.body;
-    if (!title || title === '') return res.status(400).json({ error: "Title is required" })
-    if (!author || author === '') return res.status(400).json({ error: "Author is required" })
-
-    const id = (Books.length)+1;
-    const book = { id, title, author };
-    Books.push(book)
-    console.log(Books);
-    return res.status(201).json({ Message: "Book has been added to the store Successfully!", id })
+  const id = Books.length + 1;
+  const book = { id, title, author };
+  Books.push(book);
+  console.log(Books);
+  return res
+    .status(201)
+    .json({ Message: "Book has been added to the store Successfully!", id });
 }
 
-exports.deleteBooks = (req,res)=>{
-    const id = parseInt(req.params.id);
-    if(isNaN(id)){
-        return res.status(400).json({error:"Id must be a number!"})
-    }
-    const indexToDelete = Books.findIndex((e)=> e.id === id);
+export function deleteBooks(req, res) {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Id must be a number!" });
+  }
+  const indexToDelete = Books.findIndex((e) => e.id === id);
 
-    if(indexToDelete<0){
-        return res.status(404).json({error:`The id ${id} does not exists in our database`})
-    }
+  if (indexToDelete < 0) {
+    return res
+      .status(404)
+      .json({ error: `The id ${id} does not exists in our database` });
+  }
 
-    Books.splice(indexToDelete, 1);
-    return res.status(200).json({message:`You have successfully deleted the book with id ${id}!`})
+  Books.splice(indexToDelete, 1);
+  return res
+    .status(200)
+    .json({ message: `You have successfully deleted the book with id ${id}!` });
 }
+
+export default { getAllBooks, getBookById, createBooks, deleteBooks };
--- nodejs-2026/book-store-api/routes/book.routes.js
+++ nodejs-2026/book-store-api/routes/book.routes.js
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import Controller from "../controllers/book.controller.js"
+import { Router } from "express";
+import Controller from "../controllers/book.controller.js";
 const router = Router();
 
-router.get('/', Controller.getAllBooks);
+router.get("/", Controller.getAllBooks);
 
-router.get('/:id', Controller.getBookById)
+router.get("/:id", Controller.getBookById);
 
 // Implement POST requests to add Books
 
-router.post("/", Controller.createBooks)
+router.post("/", Controller.createBooks);
 
 // delete a book by id
 
